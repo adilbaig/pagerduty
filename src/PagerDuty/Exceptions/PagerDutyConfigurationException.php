6fc4758856f463d3986a918b0bf6635072b9f7a2
jmgibson1976 <jmgibson1976@users.noreply.github.com>
2019-07-15 15:48:20 -0500

    Added configurable options to curl call (#5)
    * updated .gitignore to ignore IDE config dir
    
    * moved Exceptions into Exceptions directory;
    updated README to include new functionality;
    Added PagerDutyHttpConnection class to override send functionality in Event class - this allows for passing in custom headers and connection information as well as the ability to use proxies
    
    * added new line to end of file to supress warnings

---
+++ src/PagerDuty/Exceptions/PagerDutyConfigurationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PagerDuty\Exceptions;
+
+/**
+ * Class PagerDutyConfigurationException
+ * @package PagerDuty\Exceptions
+ */
+class PagerDutyConfigurationException extends \Exception
+{
+
+}
